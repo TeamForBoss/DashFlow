547a60934b536bd90941525c78f99461b9fa60eb
ukey77 <yujin25683@gmail.com>
2025-02-16 17:15:33 +0900

    Added and improved backend daemon environment setup

--- backend/src/daemon/dashDaemon.js
+++ backend/src/daemon/dashDaemon.js
@@ -3,19 +3,32 @@ class DashDaemon{
         this.id = id;
         this.express = null;
         this.app = null;
-        this.path = require("path");
+        this.cors = null;
+        this.path = null;
     }
     settings(){
         this.express = require("express");
         this.app = this.express();
 
-        const listenPort = () => {
-            const serverConfig = require(this.path.join(__dirname,"../config/server.config"));
-            this.app.listen(serverConfig["port"], () => {
-                console.log(`http://${serverConfig["host"]}:${serverConfig["port"]}`);
-            });
-        }
-        listenPort();
+        //cors
+        this.cors = require("cors");
+        this.app.use(this.cors());
+
+        // static
+        this.app.use(this.express.static("assets"));
+
+        //json ( == bodyParser)
+        this.app.use(this.express.json());
+        this.app.use(this.express.urlencoded({extended:false}));
+
+        //path
+        this.path = require("path");
+    }
+    startServer(){
+        const serverConfig = require(this.path.join(__dirname,"../config/server.config"));
+        this.app.listen(serverConfig["port"], () => {
+            console.log(`http://${serverConfig["host"]}:${serverConfig["port"]}`);
+        });
     }
     registerRoutes(){
         //  * @description 유진_ home 라우트
@@ -26,11 +39,11 @@ class DashDaemon{
         //  * @description 정임_ 어쩌구 페이지 라우트
 
         //  * @description 진아_ 어쩌구 페이지 라우트
-
     }
     run(){
         this.settings();
         this.registerRoutes();
+        this.startServer();
     }
 }
 const dashDaemon = new DashDaemon("dashDaemon")

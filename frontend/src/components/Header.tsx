964386b9bb342647cbfa445651577ec602578f8d
ukey77 <yujin25683@gmail.com>
2025-02-26 15:19:19 +0900

    headerValue update

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -68,7 +68,47 @@ const Header: React.FC<HeaderProps> = ({ page }) => {
     pyeongtaek: "평택시",
     hanam: "하남시",
     hwaseong: "화성시",
+  
+    // 서울특별시 구 데이터
+    gangnam: "강남구",
+    gangdong: "강동구",
+    gangbuk: "강북구",
+    gangseo: "강서구",
+    gwanak: "관악구",
+    gwangjin: "광진구",
+    guro: "구로구",
+    geumcheon: "금천구",
+    nowon: "노원구",
+    dobong: "도봉구",
+    dongdaemun: "동대문구",
+    dongjak: "동작구",
+    mapo: "마포구",
+    seodaemun: "서대문구",
+    seocho: "서초구",
+    seongdong: "성동구",
+    seongbuk: "성북구",
+    songpa: "송파구",
+    yangcheon: "양천구",
+    yeongdeungpo: "영등포구",
+    yongsan: "용산구",
+    eunpyeong: "은평구",
+    jongno: "종로구",
+    "seoul-jung": "중구",
+    jungnang: "중랑구",
+  
+    // 인천광역시 군·구 데이터 추가
+    ganghwa: "강화군",
+    gyeyang: "계양구",
+    michuhol: "미추홀구",
+    namdong: "남동구",
+    dong: "동구",
+    bupyeong: "부평구",
+    seo: "서구",
+    yeonsu: "연수구",
+    ongjin: "옹진군",
+    "incheon-jung": "중구",
   };
+  
 
   const displayText =
     page === "home"

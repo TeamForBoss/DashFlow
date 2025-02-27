a2da40799afc008b2faf77907bd20fad46c2a2e7
jomim2 <wjddla7374@naver.com>
2025-02-27 18:15:04 +0900

    footerUpdate

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,24 +1,50 @@
+import githubImg from "../assets/images/icons/footer/githubImg.png";
+import kakaoImg from "../assets/images/icons/footer/kakaoImg.png";
+import pdfImg from "../assets/images/icons/footer/pdfImg.png";
+import pdfFile from "../assets/pdf/SafetyTrafficLight.pdf";
 
 const Footer = () => {
   return (
     <footer className="footer">
-          <div className="footerWrap">
-              <p>© {new Date().getFullYear()} Safety Warning Light. ForBoss. </p>
-              <div className="fooLeft">
-                <a href="#" className="viewCode">code</a> |
-                <a href="#" className="viewPdf">pdf</a> |
-                  <p>Contact :</p>
-                <a href="#" className="contactY">Yujin</a> |
-                <a href="#" className="contactJ">Jeongim</a> |
-                <a href="#" className="contactS">Sujin</a> |
-                <a href="#" className="contactA">Jinah</a>
-              </div>
-              <div className="fooRight">
-                  <p>All rights reserved.</p>
-              </div>
+      <div className="footerWrap">
+        <p>© {new Date().getFullYear()} Safety Traffic Light. Team.ForBoss </p>
+        <div className="fooLeft">
+          <a
+            href="https://github.com/TeamForBoss/DashFlow"
+            className="viewCode"
+          >
+            <img src={githubImg} className="foImg"></img>
+          </a>{" "}
+          |
+          <a href={pdfFile} className="viewPdf">
+            <img src={pdfImg} className="foImg"></img>
+          </a>{" "}
+          |
+          <p>
+            Contact : <img src={kakaoImg} className="foImg"></img>
+          </p>
+          <a href="https://open.kakao.com/o/sXZSHZZg" className="contactY">
+            Yujin
+          </a>{" "}
+          |
+          <a href="https://open.kakao.com/o/snoCRGfh" className="contactJ">
+            Jeongim
+          </a>{" "}
+          |
+          <a href="https://open.kakao.com/o/sbLUmxih" className="contactS">
+            Sujin
+          </a>{" "}
+          |
+          <a href="https://open.kakao.com/o/sdONyxih" className="contactA">
+            Jinah
+          </a>
         </div>
+        <div className="fooRight">
+          <p>All rights reserved.</p>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
